746f0ccff724306d0eaaeb6f480be88e287dc351
hao wang <ge_nah@hotmail.com>
2019-05-15 00:22:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,13 +1,13 @@
 class Hash
   def keys_of(arguments)
     # code goes here
-    # array = []
+    array = []
     # hash.each do |key, value|
     #   if value == arguments
     #     array << key
     #   end
     # end
-    # array
+    array
   end
 
 end
