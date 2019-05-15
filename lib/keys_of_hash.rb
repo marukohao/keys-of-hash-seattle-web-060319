d40dc37d83df283e629562fdca52191f8d149790
hao wang <ge_nah@hotmail.com>
2019-05-15 00:35:17 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    #code goes here
+    array = []
+    arguments.each do |argu|
+      each do |key, value|
+        if value == argu
+          array << key
+        end
+      end
+    end
+    array
   end
 end
